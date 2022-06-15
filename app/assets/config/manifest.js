c13cdcd61540ca6c66bf955afd83fa6a62f74291
TDelgado <tdelgado39@gmail.com>
2022-06-14 20:49:29 -0400

    proyecto clase

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,15 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+
+
+//= require breakpoints.min.js
+//= require browser.min.js
+
+//= require jquery.dropotron.min.js
+//= require jquery.min.js
+//= require jquery.scrollex.min.js
+//= require jquery.scrolly.min.js
+//= require main.js
+//= require util.js
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+  def test
+  end
+  def login
+  end
+  def about_us
+  end
+end
